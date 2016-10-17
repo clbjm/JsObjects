033af9b27b630e81147169dc4e495e5e7cd6927a
Jonathon Mullins <jonathonm93@gmail.com>
2016-10-17 11:52:57 -0600

    update

---
+++ powerup.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class powerup {
+    int power = 10;
+
+    int duration = 5;
+
+    string name = "powerup";
+
+    public void runpowerup () {
+        Console.WriteLine("Using powerup")
+    }
+}
