e026223b6c1959180df63cdf8cad4e0662ace871
Jonathon Mullins <jonathonm93@gmail.com>
2016-11-11 11:06:05 -0700

    better update

--- Scriptcs/project/Game.cs
+++ Scriptcs/project/Game.cs
@@ -1,85 +1,91 @@
 using System;
 public class Game {
-public static bool canPlay = true;
-public Game () {
-    Health.power = 100;
-    Health.message = "You are getting stronger.";
-    Ammo.message = "You have more ammo";
-    Cave.StartMessage = "You have entered a cave";
-    UnderWater.objects = new string[] {"SeaWead", "Coral", "Fish", "Shark"};
-}
+
+    public static Action StartGame;
+
+    public static bool canPlay = true;
+
+    public Game1 () {
+        Health.power = 100;
+        Health.message = "You are getting stronger.";
+        Ammo.message = "You have more ammo";
+        Cave.StartMessage = "You have entered a cave.";
+        UnderWater.objects = new string[] {"SeaWead", "Coral", "Fish", "Shark"};
+    }
 
     //Runs at the start of the game
     public void Start (){
-    Console.WriteLine("Please type in your name:");
-    name = Console.ReadLine();
-    Console.WriteLine("Your player name is " + myGame.name);
-    while(Game.canPlay) {
-        GameTimer();
-        Play();
-    }
-    Console.WriteLine("You died");
-    Console.WriteLine("Game Over");
-    }
-    /*
-    After prompt the game for a name we:
-    Enter a cave
-    find Health.
-    Meet a dragon. (need an enemy class)
-    pick a weapon.
-    Battle Dragon.
-    If we win: get Health and Ammo
-    If dragon wins: loose Health.
-    */
-}
-private void Play(){
-    Console.WriteLine("Play commands: Play, End, Help");
-           // gameState = Console.ReadLine();
-    switch (GameStateMachine.currentGameState)
-    {
-        case GameStateMachine.GameStates.End:
+        StartGame();
+        Console.WriteLine("Please type in your name:");
+        name = Console.ReadLine();
+        Console.WriteLine("Your Player Name is " + name);
+        Cave.Enter();        
+        while(Game.canPlay) {
+            GameTimer();
+            Play();
+        }
+        Console.WriteLine("You Died");
         Console.WriteLine("Game Over");
-        Environment.Exit(0);
-        break;
-
-        case GameStateMachine.Help:
-            Console.WriteLine("What do you need help for. If you can't play this game, you have issues.");
-            Play();
-        break;
-
-        case GameStateMachine.GameStates.Play:
-
-        break;
-
-        default:
-            Console.WriteLine(" is not a valid option.");
-            Play();
-        break;
     }
+
+    
+    private void Play (){
+        Console.WriteLine("Play commands: Play, End, Help");
+    // gameState = Console.ReadLine();
+
+        switch (GameStateMachine.currentGameState)
+        {
+
+            case GameStateMachine.GameStates.End:
+                Console.WriteLine("Game Over");
+                Environment.Exit(0);
+                break;
+
+            case GameStateMachine.GameStates.Help:
+                Console.WriteLine("What do you need help for. If you can't play this game, you have issues.");
+                Play();
+            break;
+            
+            case GameStateMachine.GameStates.Play:
+
+            break;
+
+            default:
+                Console.WriteLine(" is not a valid option.");
+                Play();
+            break;
+        }
+        
         Random randomNum = new Random();
-        Cave.Encounter(randomNum.Next(0, Cave.objects.Length), "Walked");
+        Cave.Encounter(randomNum.Next(0, Cave.objects.Length), "walked");
     }
 
-public static void GameTimer () {
-        System.Threading.Thread.Sleep(2000);
-}
+    public static void GameTimer () {
+         System.Threading.Thread.Sleep(2000);
+    }
 
-
-//Game Levels 
-private LevelBase Cave = new LevelBase();
-public static LevelBase UnderWater = new LevelBase();
-//Game PowerUps
-public PowerUpBase Health = new PowerUpBase();
-public PowerUpBase Ammo = new PowerUpBase();
-
-private bool canPlay = true;
-//Game Weapons
-private WeaponBase Gun = new WeaponBase();
-private WeaponBase Rifle = new WeaponBase();
-private WeaponBase Knife = new WeaponBase();
+    //Game Levels
+    private LevelBase Cave = new CaveLevel();
+    public static LevelBase UnderWater = new LevelBase();
+    //Game PowerUps
+    public PowerUpBase Health = new PowerUpBase();
+    public PowerUpBase Ammo = new PowerUpBase();
+    
+    //Game Weapons
+    private WeaponBase Gun = new WeaponBase();
+    private WeaponBase Rifle = new WeaponBase();
 
     public string name;
-
     private int score;
-    }
-    
+}
+
+/*
+        After prompt the game for a name we:
+        Enter a cave
+        find Health.
+        Meet a dragon. (need an enemy class)
+        Pick weapon.
+        Battle dragon. (Battle class)
+        If we win: get Health and ammo
+        if dragon wins: loose Health;
+        */
